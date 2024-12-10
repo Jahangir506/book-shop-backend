80159f0cfbaf08e76d2339b1a6fa0aaabc2ee8c3
Md. Jahangir Alam <md.jahangir.alam9649@gmail.com>
2024-12-10 17:05:43 +0600

    ts-node-dev, nodemon and  tsc --watch, script is generated in the package.json file

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ async function server() {
     try {
         await mongoose.connect(config.database_url as string);
         app.listen(config.port, () => {
-            console.log(`Example app listening on port ${config.port}`)
+            console.log(`Server running on port ${config.port} 🏃‍➡️ ➡️`)
         })
     } catch (error) {
         console.log(error)
